4c23e1ef0700e78ecf32f72182fe692a68eacec9
shaffachaudhry <134459870+shaffachaudhry@users.noreply.github.com>
2023-07-14 21:29:36 -0500

    update js/readme

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # Weather-Dashboard
 06 Server-Side APIs
+
+## Information
+User Story of this project: 
+To create a webpage that lets a user see the weather outlook (current + 5 day forecast) for multiple cities
+
+## Description
+This webpage allows users to search cities by name and be provided the current weather in the main container followed by the 5 day forecast along with the dates. The current forecast will provide the temperature, humidity, windspeed, and an icon detailing the weather. The webpage should allow a search history that a user may click on to review the forecast of that city again. 
+
+
+## Usage 
+No downloads are necessitated to utilize this weather dashboard. This is a deployed webpage application that may be used via any Web browser. 
+
+## Links
+You may access the deployed application at: 
+https://shaffachaudhry.github.io/Weather-Dashboard/
+
+you may access the repository at: 
+https://github.com/shaffachaudhry/Weather-Dashboard
+
+## Sources Utilized 
+Multiple sources were used to help in the functionality of the website to include 
+-Class Activities Module 6 Side Server API's\
+https://coding-boot-camp.github.io/full-stack/apis/how-to-use-api-keys \
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/slice \
+https://developer.mozilla.org/en-US/docs/Web/API/Element/tagName \ 
+https://www.youtube.com/watch?v=cuEtnrL9-H0&ab_channel=WebDevSimplified \
+https://developer.mozilla.org/en-US/docs/Web/API/Element/classList \
+https://stackoverflow.com/questions/1531093/how-do-i-get-the-current-date-in-javascript \
+
+## Mock Image
+![ A mock-up of the webpage](./assets/mock-up.png)
---
+++ script.js
@@ -0,0 +1,120 @@
+//API SET UP 
+var apiKey = 'fc88128cb6939ee5daf5d7b81f47e661';
+var apiUrl = 'https://api.openweathermap.org/data/2.5';
+//API forecast https://api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
+//API Weather (curent) https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+
+//setting variables 2/2 DOM
+var InputForm = document.getElementById('weather-form');
+var cityInput = document.getElementById('city-input');
+var WeatherCard = document.getElementById('current-weather-card');
+var forecastContainer = document.getElementById('forecast-container');
+var searchHistoryList = document.getElementById('search-history');
+
+// submit form via event listenr; using city for parameter instead of long/lat
+InputForm.addEventListener('submit', function (event) {
+  event.preventDefault();
+  var city = cityInput.value;
+  if (city) {
+    getWeather(city);
+    saveSearchHistory(city);
+  }
+});
+
+// etablish search history items via event listener 
+searchHistoryList.addEventListener('click', function (event) {
+  if (event.target.tagName === 'LI') {
+    var city = event.target.textContent;
+    getWeather(city);
+  }
+});
+
+//fetch current weather / use destrucuring assignment to obtain values from object
+function getCurrentWeather(city) {
+  var currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  fetch(currentWeatherUrl)
+    .then(response => response.json())
+    .then(data => {
+      var { name, sys, main, weather, wind } = data;
+      var country = sys.country;
+      var temperature = main.temp;
+      var humidity = main.humidity;
+      var windSpeed = wind.speed;
+      var weatherIcon = weather[0].icon;
+
+      // add to current weather car 
+      WeatherCard.innerHTML = `<h2>${name}, ${country}</h2>
+        <p>Date: ${getCurrentDate()}</p>
+        <p>Temperature: ${temperature}°C</p>
+        <p>Humidity: ${humidity}%</p>
+        <p>Wind Speed: ${windSpeed} m/s</p>
+        <img src="http://openweathermap.org/img/w/${weatherIcon}.png" alt="Weather Icon" width = "100px" height = "100px" >`;
+    })
+}
+
+// fetch forecast weather 
+function getForecastWeather(city) {
+  var forecastWeatherUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+  fetch(forecastWeatherUrl)
+    .then(response => response.json())
+    .then(data => {
+      var forecastList = data.list;
+      forecastContainer.innerHTML = '';
+
+      // forecast cards
+      for (let i = 0; i < forecastList.length; i += 8) {
+        var forecast = forecastList[i];
+        var { dt, main, weather } = forecast;
+        var date = new Date(dt * 1000);
+        var temperature = main.temp;
+        var humidity = main.humidity;
+        var weatherIcon = weather[0].icon;
+
+        // make a forecast card
+        var card = document.createElement('div');
+        card.classList.add('forecast-card');
+        card.innerHTML = `<h3>${getFormattedDate(date)}</h3>
+          <img src="http://openweathermap.org/img/w/${weatherIcon}.png"alt="Weather Icon" >
+          <p>Temperature: ${temperature}°C</p>
+          <p>Humidity: ${humidity}%</p>`;
+        forecastContainer.appendChild(card);
+      }
+    })
+}
+
+// fetch weather data
+function getWeather(city) {
+  getCurrentWeather(city);
+  getForecastWeather(city);
+}
+
+// obtain  current date
+function getCurrentDate() {
+  var currentDate = new Date();
+  return getFormattedDate(currentDate);
+}
+
+// date string
+function getFormattedDate(date) {
+  var options = { year: 'numeric', month: 'long', day: 'numeric' };
+  return date.toLocaleDateString(undefined, options);
+}
+
+// save search history
+function saveSearchHistory(city) {
+  let searchHistory = JSON.parse(localStorage.getItem('searchHistory')) || [];
+  if (!searchHistory.includes(city)) {
+    searchHistory.push(city);
+  }
+  localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
+
+  displaySearchHistory();
+}
+function displaySearchHistory() {
+  searchHistoryList.innerHTML = '';
+  var searchHistory = JSON.parse(localStorage.getItem('searchHistory')) || [];
+    var listItem = document.createElement('li');
+    listItem.textContent = city;
+    searchHistory.appendChild(listItem);
+  }
+displaySearchHistory();
